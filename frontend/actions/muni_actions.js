5b60f6c4c906a6e6a0d60d39e9bd6bd1e856498d
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-17 19:42:37 -0800

    more actions

--- frontend/actions/muni_actions.js
+++ frontend/actions/muni_actions.js
@@ -2,22 +2,46 @@ import * as MuniUtil from "../util/muni_util";
 
 export const RECEIVE_ALL_MUNI_LINES = "RECEIVE_ALL_MUNI_LINES";
 export const RECEIVE_MUNI_LINE = "RECIEVE_MUNI_LINE";
-export const RECEIVE_MUNI_STOPS = "RECEIVE_MUNI_STOPS";
-export const RECEIEVE_MUNI_STOP = "RECEIVE_MUNI_STOP";
+export const RECEIVE_ALL_MUNI_STOPS = "RECEIVE_ALL_MUNI_STOPS";
+export const RECEIVE_MUNI_STOP = "RECEIVE_MUNI_STOP";
 export const RECEIVE_MUNI_PREDICTIONS = "RECEIVE_MUNI_PREDICTIONS";
 
 export const fetchLines = () => dispatch => (
-  MuniUtil.fetchLines().then((lines) => dispatch(receiveAllLines(lines)))
+  MuniUtil.fetchLines().then((lines) => dispatch(receiveAllMuniLines(lines)))
 )
 
 export const fetchLine = (id) => dispatch => (
-  MuniUtil.fetchLine(id).then((line) => dispatch(receiveLine(line)))
+  MuniUtil.fetchLine(id).then((line) => dispatch(receiveMuniLine(line)))
 )
 
 export const fetchStops = () => dispatch => (
-  MuniUtil.fetchStops().then((stops) => dispatch(receiveAllStops(stops)))
+  MuniUtil.fetchStops().then((stops) => dispatch(receiveAllMuniStops(stops)))
 )
 
-export const fetchStop = (id) => dispatch =. (
-  MuniUtil.fetchStops(id).then((stop) => dispatch(receiveStop(stop)))
+export const fetchStop = (id) => dispatch => (
+  MuniUtil.fetchStops(id).then((stop) => dispatch(receiveMuniStop(stop)))
 )
+
+export const fetchPredictionsAtStop = (id) => dispatch => (
+  MuniUtil.fetchPredictionsAtStop(id).then((predictions) => dispatch(receiveMuniPredictions(predictions)))
+)
+
+const receiveAllLines = lines => ({
+  type: RECEIVE_ALL_MUNI_LINES,
+  lines
+})
+
+const receiveMuniLine = line => ({
+  type: RECEIVE_MUNI_LINE,
+  line
+})
+
+const receiveAllMuniStops = stops => ({
+  type: RECEIVE_ALL_MUNI_STOPS,
+  stops
+})
+
+const receiveMuniStop = stop => ({
+  type: RECEIVE_MUNI_STOP,
+  stop
+})
