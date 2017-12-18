76326f368d49fd7f74c1b1facfe5aa8e9f27a4f8
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-17 17:37:05 -0800

    api utils

---
+++ frontend/util/muni_util.js
@@ -0,0 +1,23 @@
+// Mission and Powers stopCode: 17841
+let APIKey = "d90713e6-a4b5-4799-a6cd-fb74f3be460b"
+
+export const fetchLines = () => {
+  return $.ajax({
+    "method": "GET",
+    "url": `http://api.511.org/transit/lines?api_key=${APIKey}&operator_id=SF`
+  })
+}
+
+export const fetchLine = (id) => {
+  return $.ajax({
+    "method": "GET",
+    "url": ` http://api.511.org/transit/lines?api_key=${APIKey}&operator_id=SF&Line_id=${id}`
+  })
+}
+
+export const fetchPredictionsAtStop = (id) => {
+  return $.ajax({
+    "method": "GET",
+    "url": `http://api.511.org/transit/StopMonitoring?api_key=${APIKey}&agency=SF&stopCode=${id}`
+  })
+}
