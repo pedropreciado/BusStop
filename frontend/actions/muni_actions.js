c4c1d722455e390532c4ebff6c04edd7731c06ec
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-17 18:47:10 -0800

    actions

--- frontend/actions/muni_actions.js
+++ frontend/actions/muni_actions.js
@@ -1,3 +1,23 @@
 import * as MuniUtil from "../util/muni_util";
 
-export const 
+export const RECEIVE_ALL_MUNI_LINES = "RECEIVE_ALL_MUNI_LINES";
+export const RECEIVE_MUNI_LINE = "RECIEVE_MUNI_LINE";
+export const RECEIVE_MUNI_STOPS = "RECEIVE_MUNI_STOPS";
+export const RECEIEVE_MUNI_STOP = "RECEIVE_MUNI_STOP";
+export const RECEIVE_MUNI_PREDICTIONS = "RECEIVE_MUNI_PREDICTIONS";
+
+export const fetchLines = () => dispatch => (
+  MuniUtil.fetchLines().then((lines) => dispatch(receiveAllLines(lines)))
+)
+
+export const fetchLine = (id) => dispatch => (
+  MuniUtil.fetchLine(id).then((line) => dispatch(receiveLine(line)))
+)
+
+export const fetchStops = () => dispatch => (
+  MuniUtil.fetchStops().then((stops) => dispatch(receiveAllStops(stops)))
+)
+
+export const fetchStop = (id) => dispatch =. (
+  MuniUtil.fetchStops(id).then((stop) => dispatch(receiveStop(stop)))
+)
